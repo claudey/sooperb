8cbed8851975e980adfd872a58c8ab93575998e8
Claude Ayitey <clauditey@gmail.com>
2013-08-23 03:39:18 +0000

    some fixes

--- web.js
+++ web.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+// var app = express.createServer(express.logger());
+var app = express();
+
+app.configure(function() {
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.user(express.bodyParser());
+	app.use(express.logger("short"));
+});
 
 app.get('/', function(request, response) {
   var data = fs.readFileSync('index.html').toString()
@@ -9,7 +16,7 @@ app.get('/', function(request, response) {
   
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 1000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
